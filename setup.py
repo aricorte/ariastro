acc1f4029c9dcfdfe7b41575f605ccc1e2b8d5fd
Julio Trevisan <juliotrevisan@gmail.com>
2018-02-28 16:53:58 -0300

    version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from glob import glob
 setup(
     name = 'ariastro',
     packages = find_packages(),
-    version = '0.18.02.28.0',
+    version = '0.18.02.28.1',
     license = 'GNU GPLv3',
     platforms = 'any',
     description = 'Ari Astro tools',
